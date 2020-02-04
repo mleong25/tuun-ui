995f388ba9ea2976e7c20c3d0fbe62f4fac987be
Marcus Leong <mwleong25@gmail.com>
2020-02-04 17:11:46 -0600

    setting and genre fields added

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 import '../styles/Create.css';
  
@@ -14,7 +15,31 @@ class Create extends Component {
                     Setting
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="0">
-                <Card.Body className="cardBody">Pick from the setting selection below:</Card.Body>
+                <Card.Body className="cardBody">
+                    Pick from the setting selection below:
+                    <div className="selectionContainer">
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Hangout</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Party</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Dinner</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Ambient</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Card.Body>
                 </Accordion.Collapse>
             </Card>
             <Card>
@@ -22,7 +47,55 @@ class Create extends Component {
                     Genre
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="1">
-                <Card.Body className="cardBody">Pick from the genre selection below:</Card.Body>
+                <Card.Body className="cardBody">Pick from the genre selection below:
+                    <div className="selectionContainer">
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Pop</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Rock</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Hip-Hop</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Jazz</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Classical</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Workout</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Study</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Country</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card className="selection">
+                            <Card.Body>
+                                <Card.Title>Gaming</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Card.Body>
                 </Accordion.Collapse>
             </Card>
         </Accordion>
