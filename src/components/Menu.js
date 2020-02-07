b702142f3942ab6e7964148f39ee67462b13cc42
Miller Bath <millerbath@Millers-MacBook-Pro.local>
2020-02-07 12:32:24 -0600

    Changed up nav bar a bit. added comments.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/Menu.css';
 import { Navbar, Nav,  } from 'react-bootstrap';
-import MenuIcon from "@material-ui/icons/Menu";
+//import MenuIcon from "@material-ui/icons/Menu";
 
 class Menu extends Component {
   render() {
@@ -9,12 +9,21 @@ class Menu extends Component {
       <div className="Menu">
         <Navbar fixed="top" bg="dark" variant="dark">
           <Navbar.Brand className="brand" href="/">
-            Tuun <MenuIcon></MenuIcon>
+            <img
+              src="favicon.ico" //can't figure out how to access the image from public/favicon.ico... in same dir for right now.
+              width="25"
+              height="25"
+              alt="tuun logo"
+            />
+            &nbsp;&nbsp;
+            Tuun
           </Navbar.Brand>
           <Nav className="justify-content-center navs" fill>
             <Nav.Item><Nav.Link href="/create">Create</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link href="/playlists">Playlists</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link href="/connect">Connect</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link className="login" href="/login">Login</Nav.Link></Nav.Item> 
+              {/*broken href... cant shift login over to right in menu.css */}
           </Nav>
         </Navbar>
       </div>
