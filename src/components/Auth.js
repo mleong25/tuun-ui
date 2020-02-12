0e8cd24e25f112c5473a3836fe51638f74f3c4f2
wittmanalex <wittmanalex@gmail.com>
2020-02-11 19:21:14 -0600

    Added necessary scopes for web player.

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,55 +1,55 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import '../styles/Auth.css';
-import uuid from 'uuid'
+import uuid from 'uuid';
 
 const params = new URLSearchParams({
-  client_id: '1db68b4b02f148b39f419f3b942452ed', // Your client id
-  redirect_uri: 'http://localhost:3000/create', // Your redirect uri
-  scope: 'user-read-private%20user-read-email',
-  response_type: 'token',
-  state: uuid()
+    client_id: '1db68b4b02f148b39f419f3b942452ed', // Your client id
+    redirect_uri: 'http://localhost:3000/create', // Your redirect uri
+    scope: 'streaming%20user-read-email%20user-modify-playback-state%20user-read-private%20user-read-playback-state%20user-read-currently-playing%20app-remote-control%20playlist-read-collaborative%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-library-modify%20user-library-read%20user-top-read%20user-read-recently-played',
+    response_type: 'token',
+    state: uuid()
 });
 
 const OauthURL = `https://accounts.spotify.com/authorize?${params}`;
 
 class Auth extends Component {
-  componentDidMount() {
-    // TODO: handle user errors here
-    const params = new URLSearchParams(window.location.hash.replace('#', ''))
+    componentDidMount() {
+        // TODO: handle user errors here
+        const params = new URLSearchParams(window.location.hash.replace('#', ''));
 
-    const token = params.get('access_token')
+        const token = params.get('access_token');
 
-    if(token) {
-      window.localStorage.setItem('token', token)
+        if (token) {
+            window.localStorage.setItem('token', token);
 
-      window.location.pathname = '/'
+            window.location.pathname = '/';
+        }
     }
-  }
 
-  render() {
-    return(
-    <div className="landing-page-container">
-      <h1 className="App-header">
-      Welcome to T<span style={{ color: "#6C2EB9" }}>u</span>un
-      </h1>
-      <Button
-      variant="primary"
-      className="App-link white btn-lg"
-      onClick={() => {
-        window.location.href = OauthURL
-      }}>
-      <img
-      src="spotifylogo.jpg" //can't figure out how to access the image from public/favicon.ico... in same dir for right now.
-      width="50"
-      height="50"
-      alt="tuun logo"
-      />
-      Login with Spotify
-      </Button>
-    </div>
-    );
-  }
+    render() {
+        return (
+            <div className='landing-page-container'>
+                <h1 className='App-header'>
+                    Welcome to T<span style={{ color: '#6C2EB9' }}>u</span>un
+                </h1>
+                <Button
+                    variant='primary'
+                    className='App-link white btn-lg'
+                    onClick={() => {
+                        window.location.href = OauthURL;
+                    }}>
+                    <img
+                        src='spotifylogo.jpg' //can't figure out how to access the image from public/favicon.ico... in same dir for right now.
+                        width='50'
+                        height='50'
+                        alt='tuun logo'
+                    />
+                    Login with Spotify
+                </Button>
+            </div>
+        );
+    }
 }
 
-export default Auth
+export default Auth;
