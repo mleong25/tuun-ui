cf26bd8a735587c7f7d6f64c05b2f50496b9d7ed
Cbeeb121 <beaboutclayton@gmail.com>
2020-04-07 21:21:25 -0500

    updated auth.js as it was on SaveLoadPlaylists branch

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,62 +2,31 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import '../styles/Auth.css';
 import uuid from 'uuid';
-import { clientID, clientSecret } from '../Secrets';
-import axios from 'axios';
-import queryString from 'query-string';
+import { clientID } from '../Secrets';
 
-//TODO: Might have to show dialog to true/false to see what that does?
-//https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
 const params = new URLSearchParams({
     client_id: clientID, // Your client id
-    response_type: 'code',
     redirect_uri: 'http://localhost:3000/create', // Your redirect uri
-    state: uuid(),
     scope: 'streaming%20user-read-email%20user-modify-playback-state%20user-read-private%20user-read-playback-state%20user-read-currently-playing%20app-remote-control%20playlist-read-collaborative%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-library-modify%20user-library-read%20user-top-read%20user-read-recently-played',
+    response_type: 'token',
+    state: uuid()
 });
 
 const OauthURL = `https://accounts.spotify.com/authorize?${params}`;
 
 class Auth extends Component {
     componentDidMount() {
-      // ~~that new new auth code type shit~~
-      //QUERY RESPONSE GIVES BACK A CODE AND A STATE if user accepts login
-        //CODE CAN BE EXCHANGED FOR A TOKEN
-        // TODO: !! handle user errors here, verify state value is consistent (??)
-        // const params = new URLSearchParams(window.location.hash.replace('#', '')); //this is fucked up?
-        const params = new URLSearchParams(window.location);
+        // TODO: handle user errors here
+        const params = new URLSearchParams(window.location.hash.replace('#', ''));
 
-        if(window.location.search !== "") {
-          //we have a response
-          if(window.location.search[1] === "c"){
-            //proper response, now parse
-            const auth_code = window.location.search.split("=")[1].split("&")[0];
-            console.log("auth code:", auth_code);
-            //exchanging the auth_code for an access_token
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+        const token = params.get('access_token');
+        console.log(params.toString())
 
-            var urlencoded = new URLSearchParams();
-            urlencoded.append("grant_type", "authorization_code");
-            urlencoded.append("code", auth_code);
-            urlencoded.append("redirect_uri", "http://localhost:3000/create");
-            urlencoded.append("client_id", clientID);
-            urlencoded.append("client_secret", clientSecret);
+        if (token) {
+            console.log("hi", params.toString())
+            window.localStorage.setItem('token', token);
 
-            var requestOptions = {
-              method: 'POST',
-              headers: myHeaders,
-              body: urlencoded,
-              redirect: 'follow'
-            };
-
-            fetch("https://accounts.spotify.com/api/token", requestOptions)
-              .then(response => response.text())
-              .then(result => console.log(result))
-              .catch(error => console.log('error', error));
-
-
-          }
+            window.location.pathname = '/';
         }
     }
 
