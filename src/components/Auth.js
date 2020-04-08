0c235d2d82e5e400025ab6429d8b2db17784874d
wittmanalex <wittmanalex@gmail.com>
2020-04-07 21:27:45 -0500

    Changed secrets folder to process.env.

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import '../styles/Auth.css';
 import uuid from 'uuid';
-import { clientID, clientSecret } from '../Secrets';
+// import { clientID, clientSecret } from '../Secrets';
 import axios from 'axios';
 import queryString from 'query-string';
+
+const clientID = process.env.clientID;
+const clientSecret = process.env.clientSecret;
 
 //TODO: Might have to show dialog to true/false to see what that does?
 //https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
@@ -20,63 +23,62 @@ const OauthURL = `https://accounts.spotify.com/authorize?${params}`;
 
 class Auth extends Component {
     componentDidMount() {
-      // ~~that new new auth code type shit~~
-      //QUERY RESPONSE GIVES BACK A CODE AND A STATE if user accepts login
+        // ~~that new new auth code type shit~~
+        //QUERY RESPONSE GIVES BACK A CODE AND A STATE if user accepts login
         //CODE CAN BE EXCHANGED FOR A TOKEN
         // TODO: !! handle user errors here, verify state value is consistent (??)
         // const params = new URLSearchParams(window.location.hash.replace('#', '')); //this is fucked up?
         const params = new URLSearchParams(window.location);
 
-        if(window.location.search !== "") {
-          //we have a response
-          if(window.location.search[1] === "c"){
-            //proper response, now parse
-            const auth_code = window.location.search.split("=")[1].split("&")[0];
-            console.log("auth code:", auth_code);
-            //exchanging the auth_code for an access_token
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+        if (window.location.search !== '') {
+            //we have a response
+            if (window.location.search[1] === 'c') {
+                //proper response, now parse
+                const auth_code = window.location.search.split('=')[1].split('&')[0];
+                console.log('auth code:', auth_code);
+                //exchanging the auth_code for an access_token
+                var myHeaders = new Headers();
+                myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
 
-            var urlencoded = new URLSearchParams();
-            urlencoded.append("grant_type", "authorization_code");
-            urlencoded.append("code", auth_code);
-            urlencoded.append("redirect_uri", "http://localhost:3000/create");
-            urlencoded.append("client_id", clientID);
-            urlencoded.append("client_secret", clientSecret);
+                var urlencoded = new URLSearchParams();
+                urlencoded.append('grant_type', 'authorization_code');
+                urlencoded.append('code', auth_code);
+                urlencoded.append('redirect_uri', 'http://localhost:3000/create');
+                urlencoded.append('client_id', clientID);
+                urlencoded.append('client_secret', clientSecret);
 
-            var requestOptions = {
-              method: 'POST',
-              headers: myHeaders,
-              body: urlencoded,
-              redirect: 'follow'
-            };
+                var requestOptions = {
+                    method: 'POST',
+                    headers: myHeaders,
+                    body: urlencoded,
+                    redirect: 'follow',
+                };
 
-            fetch("https://accounts.spotify.com/api/token", requestOptions)
-              .then(response => response.text())
-              .then(result => console.log(result))
-              .catch(error => console.log('error', error));
-
-
-          }
+                fetch('https://accounts.spotify.com/api/token', requestOptions)
+                    .then((response) => response.text())
+                    .then((result) => console.log(result))
+                    .catch((error) => console.log('error', error));
+            }
         }
     }
 
     render() {
         return (
-          <Button
-              variant='primary'
-              className='App-link white btn-lg'
-              onClick={() => {
-                  window.location.href = OauthURL; console.log("Applying new URL...");
-              }}>
-              <img
-                  src='spotifylogo.jpg' //can't figure out how to access the image from public/favicon.ico... in same dir for right now.
-                  width='50'
-                  height='50'
-                  alt='tuun logo'
-              />
-              Login with Spotify
-          </Button>
+            <Button
+                variant='primary'
+                className='App-link white btn-lg'
+                onClick={() => {
+                    window.location.href = OauthURL;
+                    console.log('Applying new URL...');
+                }}>
+                <img
+                    src='spotifylogo.jpg' //can't figure out how to access the image from public/favicon.ico... in same dir for right now.
+                    width='50'
+                    height='50'
+                    alt='tuun logo'
+                />
+                Login with Spotify
+            </Button>
         );
     }
 }
